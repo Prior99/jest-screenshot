b5953b9ce927f5b4aeea0dd27d8a90ce032cdf25
Frederick Gnodtke <frederick@gnodtke.net>
2018-04-14 01:11:38 +0200

    Move error message into bulma message

--- src/report-viewer/components/snapshot/snapshot.tsx
+++ src/report-viewer/components/snapshot/snapshot.tsx
@@ -5,8 +5,9 @@ import * as classNames from "classnames/bind";
 import { FailedSnapshotInfo } from "../../../reporter-types";
 import { LevelItem } from "../level-item";
 import { ImageDiffViewer } from "../../image-diff-viewer";
+import * as css from "./snapshot.scss";
 
-const cx = classNames.bind(bulma);
+const cx = classNames.bind({ ...bulma, ...css });
 
 export interface SnapshotProps {
     snapshot: FailedSnapshotInfo;
@@ -24,9 +25,13 @@ export class Snapshot extends React.Component<SnapshotProps> {
                     <LevelItem name="Changed Pixels" value={String(this.snapshot.changedPixels)}/>
                     <LevelItem name="Difference" value={`${(this.snapshot.changedRelative * 100).toFixed(2)}%`}/>
                 </nav>
-                <Ansi>
-                    {this.snapshot.message}
-                </Ansi>
+                <div className={cx("message", "test-message")}>
+                    <div className={cx("message-body")}>
+                        <Ansi>
+                            {this.snapshot.message}
+                        </Ansi>
+                    </div>
+                </div>
                 <ImageDiffViewer
                     received={receivedPath}
                     snapshot={snapshotPath}

3288a15aceacbcf97e66499d56a4a63155f7a34b
Frederick Gnodtke <frederick@gnodtke.net>
2018-04-17 18:29:14 +0200

    Add tests for DiffSideBySide

---
+++ src/report-viewer/components/image-diff-viewer/diff-side-by-side/__tests__/test-diff-side-by-side.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+import { DiffSideBySide } from "..";
+
+describe("DiffSideBySide", () => {
+    it("looks as expected", () => {
+        expect(shallow(
+            <DiffSideBySide
+                received="reports/something/received.png"
+                snapshot="reports/something/snapshot.png"
+                diff="reports/something/diff.png"
+                width={100}
+                height={100}
+            />,
+        )).toMatchSnapshot();
+    });
+
+    [0, 1, 0.7].forEach(opacity => {
+        it(`when setting the diff opacity to ${opacity}`, () => {
+            const element = shallow(
+                <DiffSideBySide
+                    received="reports/something/received.png"
+                    snapshot="reports/something/snapshot.png"
+                    diff="reports/something/diff.png"
+                    width={100}
+                    height={100}
+                />,
+            );
+            const input = element.find("input[type='range']");
+            input.simulate("change", { currentTarget: { value: opacity }});
+            expect(element).toMatchSnapshot();
+            [
+                element.find("div.viewer-container div.viewer-received div.viewer-diff img"),
+                element.find("div.viewer-container div.viewer-snapshot div.viewer-diff img"),
+            ].forEach(viewerDiffImg => {
+                expect(viewerDiffImg.prop("style").opacity).toBe(opacity);
+            });
+        });
+    });
+});

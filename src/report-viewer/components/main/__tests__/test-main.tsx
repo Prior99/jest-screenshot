9309a3c43de47be548f13ab1e3008a3cbb815e6f
Frederick Gnodtke <frederick@gnodtke.net>
2018-04-17 21:49:01 +0200

    Add unit tests for Main

---
+++ src/report-viewer/components/main/__tests__/test-main.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+import { Main } from "..";
+import { StoreUi } from "../../../store";
+
+describe("Main", () => {
+    it("looks as expected", () => {
+        expect(shallow(<Main />)).toMatchSnapshot();
+    });
+
+    it("toggles the menu visibility", () => {
+        const element = shallow(<Main />);
+        expect(element.find("main").hasClass("is-9")).toBe(true);
+        expect(element.find("main").hasClass("is-12")).toBe(false);
+
+        tsdi.get(StoreUi).toggleMenu();
+        element.update();
+        expect(element).toMatchSnapshot();
+        expect(element.find("main").hasClass("is-9")).toBe(false);
+        expect(element.find("main").hasClass("is-12")).toBe(true);
+
+        tsdi.get(StoreUi).toggleMenu();
+        element.update();
+        expect(element).toMatchSnapshot();
+        expect(element.find("main").hasClass("is-9")).toBe(true);
+        expect(element.find("main").hasClass("is-12")).toBe(false);
+    });
+});

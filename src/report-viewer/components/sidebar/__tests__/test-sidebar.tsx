8a75bb392a53a97892276b17030d28fe9dab0e64
Frederick Gnodtke <frederick@gnodtke.net>
2018-04-18 09:56:14 +0200

    Add missing unit test for Sidebar

--- src/report-viewer/components/sidebar/__tests__/test-sidebar.tsx
+++ src/report-viewer/components/sidebar/__tests__/test-sidebar.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import { shallow } from "enzyme";
 import { Sidebar } from "..";
+import { StoreUi } from "../../../store";
 
 describe("Sidebar", () => {
     it("looks as expected", () => {
+        expect(shallow(<Sidebar />)).toMatchSnapshot();
+    });
+
+    it("isn't rendered with the sidebar hidden", () => {
+        tsdi.get(StoreUi).toggleMenu();
         expect(shallow(<Sidebar />)).toMatchSnapshot();
     });
 });

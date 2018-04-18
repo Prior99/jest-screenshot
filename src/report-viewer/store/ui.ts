8dcff4428672e63148615d231c14cd3b4d2e6168
Frederick Gnodtke <frederick@gnodtke.net>
2018-04-18 09:59:32 +0200

    Use bind decorator from lodash-decorators instead of action.bound

--- src/report-viewer/store/ui.ts
+++ src/report-viewer/store/ui.ts
@@ -1,12 +1,13 @@
 import { component } from "tsdi";
 import { observable, action, computed } from "mobx";
 import { FileReport } from "../../reporter-types";
+import { bind } from "lodash-decorators";
 
 @component
 export class StoreUi {
     @observable public menuVisible = true;
 
-    @action.bound public toggleMenu() {
+    @bind @action public toggleMenu() {
         this.menuVisible = !this.menuVisible;
     }
 }
